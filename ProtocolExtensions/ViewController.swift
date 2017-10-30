d6d9f1da20c52ddeda41381a2f36ee40db7f6fbb
prashanthspatil <Prashanth@Prashanths-MacBook-Pro.local>
2017-10-30 18:17:00 +0530

    Added Reachability class

--- ProtocolExtensions/ViewController.swift
+++ ProtocolExtensions/ViewController.swift
@@ -12,14 +12,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if currentReachabilityStatus != .notReachable {
+            
+        } else if currentReachabilityStatus == .reachableViaWiFi {
+            
+        } else if currentReachabilityStatus == .reachableViaWWAN {
+            
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
